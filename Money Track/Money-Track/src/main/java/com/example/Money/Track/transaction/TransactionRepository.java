df75d4f75e04d63ba1c936972be954c639ba24ca
geoffreyj92 <110561076+geoffreyj92@users.noreply.github.com>
2023-03-23 00:23:50 -0400

    updates to the transaction repository with post method

--- Money Track/Money-Track/src/main/java/com/example/Money/Track/transaction/TransactionRepository.java
+++ Money Track/Money-Track/src/main/java/com/example/Money/Track/transaction/TransactionRepository.java
@@ -1,6 +1,13 @@
 package com.example.Money.Track.transaction;
 
+import com.example.Money.Track.budget.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    @Query("SELECT t FROM budget t WHERE t.transactionId = ?1")
+    Optional<Transaction> findTransactionId(Long id);
 }
--- Money Track/Money-Track/src/main/java/com/example/Money/Track/transaction/TransactionService.java
+++ Money Track/Money-Track/src/main/java/com/example/Money/Track/transaction/TransactionService.java
@@ -1,12 +1,17 @@
 package com.example.Money.Track.transaction;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TransactionService {
+
+    private static final Logger log = LoggerFactory.getLogger(TransactionService.class);
 
     private final TransactionRepository repository;
 
@@ -22,8 +27,25 @@ public class TransactionService {
         return repository.findById(id);
     }
 
-    public void newTransaction(Transaction transaction) {
-        //search for transaction if exists. If does not exist, try to create transaction
+    public Transaction newTransaction(Transaction transaction) {
+        //search for transaction if exists. If does not exist, try to create transaction.
+        Optional<Transaction> newTransaction = repository.findTransactionId(transaction.getId());
+
+        if (newTransaction.isPresent()) {
+            throw new IllegalStateException("transaction already exists");
+        } else if (transaction.getAmount() < 0) {
+            throw new RuntimeException("Amount must be greater than or equal to 0");
+        } else {
+            try {
+                transaction.setAmount(transaction.getAmount());
+                transaction.setDate(transaction.getDate());
+                transaction.setCategory(transaction.getCategory());
+            } catch (ResourceAccessException e) {
+                log.info(e.getMessage());
+            }
+        }
+        return transaction;
+
 
     }
 }
